5aab05e9cd0354b8293b2f6d17748c951bc4f7ff
aarol <aarol@iki.fi>
2024-03-01 20:37:30 +0200

    Initial release

--- main.ts
+++ main.ts
@@ -1,134 +1,108 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Decoration, DecorationSet, EditorView, PluginValue, ViewPlugin, ViewUpdate } from "@codemirror/view";
+import { Range, RangeSet } from '@codemirror/state'
+import { syntaxTree } from '@codemirror/language'
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class HugoHighlight extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.registerEditorExtension(ViewPlugin.fromClass(HighlightViewPlugin, { decorations: (plugin) => plugin.decorations }))
+	}
+}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+class HighlightViewPlugin implements PluginValue {
+	decorations: DecorationSet;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	constructor(view: EditorView) {
+		this.decorations = this.buildDecorations(view)
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+	update(update: ViewUpdate): void {
+		if (update.viewportChanged || update.docChanged) {
+			this.decorations = this.buildDecorations(update.view)
+		}
+	}
+
+	buildDecorations(view: EditorView): DecorationSet {
+		const decorations: Array<Range<Decoration>> = []
+
+		// tree will be iterated in such order that
+		// these variables will be reassigned when a new codeblock begins
+		let beginLineNumber = 0
+		let linesToHighlight: number[] = []
+
+		for (const { from, to } of view.visibleRanges) {
+			syntaxTree(view.state).iterate({
+				from, to,
+				enter(node) {
+					if (node.type.name.includes("HyperMD-codeblock-begin")) {
+						const line = view.state.doc.lineAt(node.from)
+
+						beginLineNumber = line.number
+						const lineText = view.state.sliceDoc(line.from, line.to)
+
+						if (/{.*}/.test(lineText)) {
+							linesToHighlight = extractHLLines(lineText)
+						} else {
+							linesToHighlight = []
+						}
 					}
+					if (node.type.name.includes("HyperMD-codeblock-bg")) {
+						const line = view.state.doc.lineAt(node.from)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+						const lineNumber = line.number - beginLineNumber
+						if (linesToHighlight.includes(lineNumber)) {
+							decorations.push(Decoration.line({ class: "hugo-line-highlight" }).range(node.from))
+						}
+					}
+				}
+			})
+		}
+
+		return RangeSet.of(decorations, true)
+	}
+}
+
+function extractHLLines(line: string): number[] {
+
+	// Match a single number
+	let match = line.match(/hl_lines="?(\d+)"?\s*/)
+	if (match) {
+		return [Number.parseInt(match[1])]
+	}
+
+	// Match an array of numbers
+	match = line.match(/hl_lines=\[(.*)\]\s*/)
+	if (!match) {
+		return []
+	}
+	const lineNumbers: number[] = []
+	// valid matches: "3", 3, "3-6"
+	// invalid matches: 3-6, '3', '3-6', "6-3"
+	const lineEntries = match[1].split(",").map(s => s.trim())
+	for (const entry of lineEntries) {
+
+		const isRange = entry.includes("-")
+		const quotedMatch = entry.match(/^"(.+)"$/)
+		if (isRange) {
+			// only accept quoted ranges
+			if (quotedMatch) {
+				const [start, end] = quotedMatch[1].split("-").map(n => Number.parseInt(n))
+
+				if (start < end) {
+					for (let i = start; i <= end; i++) {
+						lineNumbers.push(i)
+					}
 				}
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		} else {
+			// entry is a single number, which may be quoted
+			const unquoted = quotedMatch?.at(1) ?? entry
+			const num = Number.parseInt(unquoted)
+			if (!Number.isNaN(num)) {
+				lineNumbers.push(num)
+			}
+		}
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	return lineNumbers
 }
